3ba02ffd34ec788674df741ffbc3704289904c62
evanacoates <113846878+evanacoates@users.noreply.github.com>
2022-11-03 23:36:52 -0400

    Add files via upload

---
+++ script-1.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var userInput - window.prompt(How many characters would you like your password to have?)
+  var passwordLength 
+
+  console.log(userInput)
+
+  If (isNaN(passwordlength))
+  var numberlist - ["0","1","2","3","4","5","6","7","8","9"]
+  var symbollist - []
+  var lowercaselist - [
+  var uppercaselist - []
+  ]
+// Write password to the #password input
+function writePassword()
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
