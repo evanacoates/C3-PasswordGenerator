b5ac45b27e086e7fa87291106a82253f0c1b0a86
Evan <evanacoates@gmail.com>
2023-03-02 16:07:48 -0500

    Working version

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var passwordLength = 8;
-var charArray= [];
+//between 8 and 128 characters
+//lowercase, uppercase, symbols, numbers
+//write password to the #password input
+//add event listener to generate button
 
-var numberList = ["0","1","2","3","4","5","6","7","8","9"]
-var symbolList = ["?", "!", "@", "#", "$", "%", "&", "*"]
-var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var passwordLength = 8;
+var charArray = [];
+var numberList = ["0","1","2","3","4","5","6","7","8","9"];
+var symbolList = ["?", "!", "@", "#", "$", "%", "&", "*"];
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", writePassword);
 
-
-// Write password to the #password input
 function writePassword() {
   var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
@@ -23,42 +25,37 @@ function writePassword() {
     passwordText.value = "";
   }
 }
+
 function generatePassword() {
   var password = "";
   for(var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(math.random() * charArray.length);
+    var randomIndex = Math.floor(Math.random() * charArray.length);
     password = password + charArray[randomIndex];
   }
   return password;
-  }
+ }
  
-
 function getPrompts(){
   charArray = [];
 
-  passwordLength = parseInt(prompt(How many characters would you like your password to have? Choose a number between '8' and '128'));
+  passwordLength = parseInt(prompt("How many characters would you like your password to have? Choose a number between 8 and 128"));
    
-    if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
-      alert("Character length must be between 8 and 128. Please try again.");;
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+      alert("Character length must be between 8 and 128. Please try again.");
+      return false;
     }
-    return false;
-
-    if (confrim("Include lowercase letters?")){
-      charArr = charArr.concat(lowercaseList)
+  if (confirm("Include lowercase letters?")){
+      charArray = charArray.concat(lowercaseList)
     }
-    if (confrim("Include uppercase letters?")){
-      charArr = charArr.concat(uppercaseList)
+  if (confirm("Include uppercase letters?")){
+      charArray = charArray.concat(uppercaseList)
     }
-    if (confrim("Include symbols?")){
-      charArr = charArr.concat(symbolList)
+  if (confirm("Include symbols?")){
+      charArray = charArray.concat(symbolList)
     }
-    if (confrim("Include numbers?")){
-      charArr = charArr.concat(numberList)
+ if (confirm("Include numbers?")){
+      charArray = charArray.concat(numberList)
     }
-    return true;
+  return true;
   
 }
-
-
-// Add event listener to generate button
-
